1a1aa9f98f493cf83798e877755334258f0ae2a9
Jan Skrzypkowski <js.skrzypkowski@gmail.com>
2020-07-13 14:33:59 +0200

    Initial commit

---
+++ src/main/java/pl/skrzypekjan/carmenagement/CarMenagementApplication.java
@@ -0,0 +1,13 @@
+package pl.skrzypekjan.carmenagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarMenagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CarMenagementApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/skrzypekjan/carmenagement/CarMenagementApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.skrzypekjan.carmenagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarMenagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
